ba6d2d7ae9a8780b7fa37ea7ef57d87d50811329
bhoomika576 <bhoomikamudhoo@gmail.com>
2026-04-17 11:54:44 +0400

    feat: add initial payload migrations

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20260417_042459_initial from './20260417_042459_initial';
+import * as migration_20260417_075239 from './20260417_075239';
 
 export const migrations = [
   {
     up: migration_20260417_042459_initial.up,
     down: migration_20260417_042459_initial.down,
-    name: '20260417_042459_initial'
+    name: '20260417_042459_initial',
+  },
+  {
+    up: migration_20260417_075239.up,
+    down: migration_20260417_075239.down,
+    name: '20260417_075239'
   },
 ];
